d256fb50aa721cc5530028dba1ccd9e8cffcf505
Sch-Tomi <sch.tom93@gmail.com>
2020-05-24 07:54:38 +0200

    fix: classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ism',
     version='1.0.0',
     description='Interactive SSH menu',
     long_description_content_type='text/markdown',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     url='https://github.com/Sch-Tomi/ism',
     author='Sch-Tomi',
     py_modules=['ism'],
@@ -20,6 +23,10 @@ setup(
         ['questionary'],
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable'
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.6',
 )
